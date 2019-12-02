0c53db7c4bbb847800f234f8efca35778e5169fb
sven@schrodt-service.net <sven@schrodt-service.net>
2019-12-02 18:07:55 +0100

    Minor changes

--- src/P7Tools/Base/Data/Guid.php
+++ src/P7Tools/Base/Data/Guid.php
@@ -1,17 +1,16 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 /**
  * P7Tools\Base\Data\Guid
  *
  * Generic class to create and handle globally unique identifiers
  *
- * !Do not use in production until it is stable!
- *
- * @link https://github.com/svenschrodt/P7Tools
- * @author Sven Schrodt<sven@schrodt-service.net>
  * @package P7Tools
+ * @author Sven Schrodt<sven@schrodt-service.net>
+ * @version 0.1
+ * @since 2019-11-25
+ * @link https://github.com/svenschrodt/P7Tools
  * @license https://github.com/svenschrodt/P7Tools/blob/master/LICENSE.md
  * @copyright Sven Schrodt<sven@schrodt-service.net>
- * @version 0.1
  */
 namespace P7Tools\Base\Data;
 
@@ -19,18 +18,18 @@ class Guid
 {
 
     /**
-     * Creates a 32 byte hexadecimal global unique ID
+     * Creating a 32 byte hexadecimal global unique ID
      *
-     * @return string the unique ID
+     * @param string $strSecret
+     * @return string
      */
-    public static function getRandomGUID(string $strSecret = null) : string
+    public static function getRandomGUID(string $strSecret = null): string
     {
         $strSalt = self::getSalt();
 
-        if($strSecret != null) {
+        if ($strSecret != null) {
             return md5(md5($strSecret) + $strSalt);
         }
-
         return $strSalt;
     }
 
@@ -39,12 +38,12 @@ class Guid
      *
      * @return string the salt
      */
-    private static function getSalt() : string
+    private static function getSalt(): string
     {
         $strTime = microtime();
         $strRandom = rand(0, 32768);
         // same in MySql:
-        //      SELECT MD5(MD5(CONCAT(UNIX_TIMESTAMP(), '#',(FLOOR(0 + RAND() * 32768)))))
+        // SELECT MD5(MD5(CONCAT(UNIX_TIMESTAMP(), '#',(FLOOR(0 + RAND() * 32768)))))
         return md5($strTime . '#' . $strRandom);
     }
 
@@ -58,13 +57,12 @@ class Guid
      * @param $strSecret string
      *            optional salt
      *            
-     * @return string 
-     *            hashed string
+     * @return string hashed string
      */
-    public static function getHash(string $strValue, string $strSecret = null) : string
+    public static function getHash(string $strValue, string $strSecret = null): string
     {
         $strSalt = '';
-        if($strSecret != null) {
+        if ($strSecret != null) {
             $strSalt = md5($strSecret);
         }
         return md5($strValue + $strSalt);
